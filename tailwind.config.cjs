9aa455d0cf956e7d3378fe5583dc1e47cdfb8a1b
bajaworksco-Lucky <bajaworksco@gmail.com>
2026-02-09 18:00:00 -0800

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#0a0a0a"
+      }
+    }
+  },
+  plugins: []
+};
